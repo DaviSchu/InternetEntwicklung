5f2237d782f3a03c183839d1d2f8680be16a2a4f
David Schu <first.schu@gmail.com>
2019-06-28 18:53:43 +0200

    wrote mymath + mymathtest

---
+++ src/main/java/MyMath.java
@@ -0,0 +1,9 @@
+import org.apache.commons.math3.util.ArithmeticUtils;
+
+public class MyMath {
+
+    public int add(int a, int b) {
+        return ArithmeticUtils.addAndCheck(a,b);
+    }
+
+}
---
+++ src/test/java/MyMathTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyMathTest {
+
+    @Test
+    public void add() {
+        MyMath mm = new MyMath();
+
+        int result = 10;
+        int check = mm.add(5,5);
+
+        assertEquals(check, result);
+    }
+}
