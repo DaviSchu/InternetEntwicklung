27e5d7c8aa7dea6561dfcc531a12e5a2d5c7cf9a
Abdul Bozkurt <abdul890@hotmail.de>
2019-06-28 20:38:30 +0200

    textdatei erstellt und inhalt ausgeben lassen in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        MyMath mm = new MyMath();
+import java.io.*;
 
+public class Main {
+    public static void main(String[] args) throws IOException{
+
+        InputStream inputStream = Main.class.getResourceAsStream("/file.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while((line= reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        MyMath mm = new MyMath();
         System.out.println(mm.add(4,9));
     }
 }
