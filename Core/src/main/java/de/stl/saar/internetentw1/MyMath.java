9807f45fd668baacade4b42c53bb2e70b0c269d8
David Schu <first.schu@gmail.com>
2019-06-29 14:45:54 +0200

    asdf

--- Core/src/main/java/de/stl/saar/internetentw1/MyMath.java
+++ Core/src/main/java/de/stl/saar/internetentw1/MyMath.java
@@ -2,9 +2,9 @@ package de.stl.saar.internetentw1;
 
 import org.apache.commons.math3.util.ArithmeticUtils;
 
-public class MyMath {
+class MyMath {
 
-    public int add(int a, int b) {
+    int add(int a, int b) {
         return ArithmeticUtils.addAndCheck(a,b);
     }
 
--- Core/src/test/java/de/stl/saar/internetentw1/MyMathTest.java
+++ Core/src/test/java/de/stl/saar/internetentw1/MyMathTest.java
@@ -7,11 +7,21 @@ import static org.junit.Assert.*;
 public class MyMathTest {
 
     @Test
-    public void add() {
+    public void add1() {
         MyMath mm = new MyMath();
 
         int result = 10;
         int check = mm.add(5,5);
+
+        assertEquals(check, result);
+    }
+
+    @Test
+    public void add2() {
+        MyMath mm = new MyMath();
+
+        int result = 100;
+        int check = mm.add(73,27);
 
         assertEquals(check, result);
     }
