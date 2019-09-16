ccec7bca77aa63693ed55c1b0a5aed4cf7b9b13c
David Schu <first.schu@gmail.com>
2019-09-16 13:03:34 +0200

    hahaha

--- 3-SmartMensa/src/main/java/de/stl/saar/internetentw1/view/LoginView.java
+++ 3-SmartMensa/src/main/java/de/stl/saar/internetentw1/view/LoginView.java
@@ -6,7 +6,11 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class LoginView {
+
     private String userName;
+    private String password;
+
+
 
     public String getUserName() {
         return userName;
@@ -16,7 +20,16 @@ public class LoginView {
         this.userName = userName;
     }
 
-    public String carryOn() {
-        return "toLogin";
+    public String getPassword() {
+        return password;
     }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String login() {
+        return "toOverview";
+    }
+
 }
