4c186d153f8b70ea6d616bfe231761abf96b9e4b
Abdul Bozkurt <abdul890@hotmail.de>
2019-09-16 15:46:31 +0200

    languages

--- 3-SmartMensa/src/main/java/de/stl/saar/internetentw1/view/I18nBean.java
+++ 3-SmartMensa/src/main/java/de/stl/saar/internetentw1/view/I18nBean.java
@@ -1,4 +1,65 @@
 package de.stl.saar.internetentw1.view;
 
-public class I18nBean {
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import java.io.Serializable;
+import java.util.*;
+
+@ManagedBean
+@SessionScoped
+public class I18nBean implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+
+    private String localeCode;
+
+    private Map<String,Object> languages;
+
+    @PostConstruct
+    public void initialize() {
+        languages = new LinkedHashMap<String,Object>();
+        languages.put("Deutsch", Locale.GERMAN);
+        languages.put("English", Locale.ENGLISH);
+        localeCode = Locale.GERMAN.getLanguage();
+    }
+
+    public void onLocaleCodeChange(ValueChangeEvent valueChangeEvent) {
+        final String newLocale = valueChangeEvent.getNewValue().toString();
+
+        for (Map.Entry<String, Object> entry : languages.entrySet()) {
+            if (entry.getValue().toString().equals(newLocale)) {
+                FacesContext.getCurrentInstance()
+                        .getViewRoot().setLocale((Locale)entry.getValue());
+            }
+        }
+    }
+
+    public String getLocaleCode() {
+        return localeCode;
+    }
+
+    public void setLocaleCode(String localeCode) {
+        this.localeCode = localeCode;
+    }
+
+    public Map<String, Object> getLanguages() {
+        return languages;
+    }
+
+  /*  public Map<String> getSelectedLanguage() {
+        if (localeCode==null) {
+            for (Map.Entry<String, Object> entry : languages.entrySet()) {
+                if (entry.getValue().toString().equals(localeCode)) {
+                    return language;
+                }
+            }
+        }
+
+        return languages.get(0);
+    }
+
+   */
 }
