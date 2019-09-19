d8b4747beba9740e9a95e9c097f96b461c391a22
David Schu <first.schu@gmail.com>
2019-09-19 12:53:00 +0200

    registerView

---
+++ 3-SmartMensa/src/main/java/de/stl/saar/internetentw1/view/RegisterView.java
@@ -0,0 +1,60 @@
+package de.stl.saar.internetentw1.view;
+
+import de.stl.saar.internetentw1.dao.interfaces.UserDao;
+import de.stl.saar.internetentw1.model.Role;
+import de.stl.saar.internetentw1.model.User;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+
+@ManagedBean
+public class RegisterView {
+
+    @ManagedProperty("#{userDao}")
+    private UserDao userDao;
+
+    private String userName;
+    private String password;
+    private String confirmPassword;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public UserDao getUserDao() {
+        return userDao;
+    }
+
+    public void setUserDao(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public void register() {
+
+        Role role = new Role(2,"user");
+        User newUser = new User(1,userName,password,role);
+
+        userDao.addUser(newUser);
+    }
+
+}
