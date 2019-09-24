0ec081e4c3733b9114f22a281d69cd229e92327c
David Schu <first.schu@gmail.com>
2019-09-24 13:54:09 +0200

    das dulde ich nicht

---
+++ 3-SmartMensa/src/main/java/de/stl/saar/internetentw1/view/menuView.java
@@ -0,0 +1,43 @@
+package de.stl.saar.internetentw1.view;
+
+import de.stl.saar.internetentw1.model.Dish;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.LinkedList;
+import java.util.List;
+
+@ManagedBean
+@SessionScoped
+public class menuView {
+
+    private List<Dish> orderList = new LinkedList<>();
+    private double total;
+
+    public List<Dish> getOrderList() {
+        return orderList;
+    }
+
+    public void setOrderList(List<Dish> orderList) {
+        this.orderList = orderList;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double newTotal) {
+        total = newTotal;
+    }
+
+    public void addDish (Dish dish) {
+        orderList.add(dish);
+        total += dish.getPrice();
+    }
+
+    public void removeDish (Dish dish) {
+        orderList.remove(dish);
+        total -= dish.getPrice();
+    }
+
+}
